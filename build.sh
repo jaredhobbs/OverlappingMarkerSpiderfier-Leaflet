022d8dfc2fe83a68e492acb0e7ad5f82eb8c28db
Jared Hobbs <jaredhobbs@gmail.com>
2013-04-10 14:17:59 -0600

    remove advanced optimizations from closure compiler
    the advanced optimizations settings were causing errors

--- build.sh
+++ build.sh
@@ -9,7 +9,6 @@ OUTFILE=${OUTDIR}${OUTNAME}
 coffee --output $OUTDIR --compile ${INDIR}${INPREFIX}.coffee
 
 java -jar /usr/local/closure-compiler/compiler.jar \
-  --compilation_level ADVANCED_OPTIMIZATIONS \
   --js ${OUTDIR}${INPREFIX}.js \
   --output_wrapper '(function(){%output%}).call(this);' \
 > $OUTFILE
